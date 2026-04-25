4e9510824bb6f31bc4d97af950121e0852223fec
ArtWarakorn <junsongneanart@gmail.com>
2026-04-25 19:36:58 +0700

    ad page

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Page() {
+    return (
+        <div>Categories
+            GET: GET /api/categories
+            POST: POST /api/categories
+            PUT: PUT /api/categories/:id
+            DELETE: DELETE /api/categories/:id
+
+            Transactions
+            GET: GET /api/transactions
+            POST: POST /api/transactions
+            PUT: PUT /api/transactions/:id
+            DELETE: DELETE /api/transactions/:id
+        </div>
+    )
+}
